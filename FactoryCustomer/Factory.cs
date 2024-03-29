45223c8ba25e3f1e912b10ce4de758a15da99133
Zubayer Ahmed <zubayerahmed232@gmail.com>
2024-03-29 12:47:38 +0600

    added simple factory pattern

---
+++ FactoryCustomer/Factory.cs
@@ -0,0 +1,19 @@
+﻿using MiddleLayer;
+
+namespace FactoryCustomer
+{
+    public static class Factory
+    {
+        private static Dictionary<int, CustomerBase> custs = new Dictionary<int, CustomerBase>();
+        public static CustomerBase Create(int typeCust)
+        {
+            if (custs.Count == 0)
+            {
+                custs.Add(1, new Customer());
+                custs.Add(2, new Lead());
+            }
+
+            return custs[typeCust];
+        }
+    }
+}
--- FactoryPatten/Program.cs
+++ FactoryPatten/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FactoryCustomer;
+using MiddleLayer;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please choose a customer type, lead or customer: Press 1 for lead and 2 for customer");
+        int customerType = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Bill Amount: ");
+        decimal billAmount = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Enter Name: ");
+        var customerName = Console.ReadLine();
+        Console.Write("Enter Bill Date: ");
+        DateTime billDate = Convert.ToDateTime(Console.ReadLine());
+        Console.Write("Enter phone number: ");
+        var phoneNumber = Console.ReadLine();
+        Console.Write("Enter address: ");
+        var address = Console.ReadLine();
+
+        CustomerBase? cust = null;
+        cust = Factory.Create(customerType);
+
+        cust.CustomerName = customerName;
+        cust.BillAmount = billAmount;
+        cust.PhoneNumber = phoneNumber;
+        cust.BillDate = billDate;
+        cust.Address =address;
+
+        cust.Validate();
+    }
+}
